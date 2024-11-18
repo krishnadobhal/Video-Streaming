3cd534beec1f508f72cda5763a28a039ccc60510
krishnadobhal <krishnadobhal234@gmail.com>
2024-11-18 10:54:37 +0530

    kafka setup

---
+++ upload-service/kafka/kafka.js
@@ -0,0 +1,64 @@
+import { Kafka } from "kafkajs"
+import fs from "fs"
+import path from "path"
+import * as dotenv from "dotenv"
+
+
+dotenv.config()
+
+
+class KafkaConfig {
+    constructor() {
+        this.kafka = new Kafka({
+            clientId: "youtube uploader",
+            brokers: [process.env.BROKER],
+            ssl: {
+                ca: [fs.readFileSync(path.resolve("./ca.pem"), "utf-8")]
+            },
+            sasl: {
+                username: process.env.AIVEN_USERNAME,
+                password: process.env.AIVEN_PASS,
+                mechanism: "plain"
+            }
+        })
+        this.producer = this.kafka.producer()
+        this.consumer = this.kafka.consumer({ groupId: "youtube-uploader" })
+    }
+
+    async produce(topic, messages) {
+        try {
+            const result = await this.producer.connect()
+            console.log("kafka connected... : ", result)
+            await this.producer.send({
+                topic: topic,
+                messages: messages
+            })
+        } catch (error) {
+            console.log(error)
+        } finally {
+            await this.producer.disconnect()
+        }
+    }
+
+    async consume(topic, callback) {
+        try {
+            await this.consumer.subscribe({ topic: topic, fromBeginning: true })
+            await this.consumer.run({
+                eachMessage: async ({
+                    topic, partition, message
+                }) => {
+                    const value = message.value.toString()
+                    callback(value)
+                }
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    
+    async connectConsumer() {
+        await this.consumer.connect();
+        console.log("Consumer connected to Kafka");
+    }
+}
+export default KafkaConfig;
