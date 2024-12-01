f9e83d0ee6dedc192b6289d18e8fa53f25331bbb
krishnadobhal <krishnadobhal234@gmail.com>
2024-12-01 22:51:42 +0530

    added watch service controllers

---
+++ watch-service/controllers/watch.controller.js
@@ -0,0 +1,53 @@
+import AWS from "aws-sdk"
+import * as dotenv from "dotenv"
+dotenv.config()
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function generateSignedUrl(videoKey) {
+
+    const s3 = new AWS.S3({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: "ap-south-1"
+    });
+
+    const params = {
+        Bucket: process.env.AWS_BUCKET,
+        Key: videoKey,
+        Expires: 3600 // URL expires in 1 hour, adjust as needed
+    };
+
+    return new Promise((resolve, reject) => {
+        s3.getSignedUrl('getObject', params, (err, url) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(url);
+            }
+        });
+    });
+}
+
+const watchVideo = async (req, res) => {
+    try {
+        console.log("hello");
+        
+        const videoKey = req.query.key; // Key of the video file in S3
+        const key=await prisma.video_data.findUnique({
+            where:{
+                id:videoKey
+            }
+        })
+        const signedUrl = await generateSignedUrl(key.master);
+        console.log(videoKey)
+        console.log(signedUrl)
+        res.json({ signedUrl });
+    } catch (err) {
+        console.error('Error generating pre-signed URL:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+export default watchVideo;
