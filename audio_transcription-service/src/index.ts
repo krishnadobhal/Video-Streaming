d9f26908bccd1d01ccec7af54f36580d14dff5b2
Krishna Dobhal <krishnadobhal234@gmail.com>
2025-12-02 17:26:40 +0530

    feat: enhance Kafka message processing with job tracking and timeout handling

--- audio_transcription-service/src/index.ts
+++ audio_transcription-service/src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import KafkaConfig from "./kafka/kafka.ts"
 import { ConsumeMessage, GetSubtitle } from './service/service.ts';
-import { KafkaMessage } from './types/index.ts';
+import { JobInfo, KafkaMessagePayload } from './types/index.ts';
 import cors from "cors"
+import dotenv from "dotenv";
+import { processEachMessage } from './service/KafkaService.ts';
+
+dotenv.config();
 
 const port = process.env.PORT || 7070;
 
@@ -11,25 +15,45 @@ app.use(cors());
 const kafkaConfig = new KafkaConfig();
 
 
+const MAX_CONCURRENT_JOBS = parseInt(process.env.MAX_CONCURRENT_JOBS as string) || 1;//how many partitions to consume concurrently
 
-kafkaConfig.consume("audio", async (message) => {
-    console.log("Received message:", message.value?.toString())
-    const messageValue = message.value ? JSON.parse(message.value.toString()) as KafkaMessage : null;
-    if (messageValue) {
-        await ConsumeMessage(messageValue);
-    }
-});
+
+// Track active jobs with metadata
+const activeJobs = new Map<string, JobInfo>();
+
+
+async function startConsumer() {
+    // Ensure consumer is connected and subscribed before running
+    await kafkaConfig.consumer.connect();
+    await kafkaConfig.consumer.subscribe({ topic: "youtube", fromBeginning: false });
+
+    await kafkaConfig.consumer.run({
+        autoCommit: false, // Disable auto commit
+        partitionsConsumedConcurrently: MAX_CONCURRENT_JOBS,
+
+        // Use autoCommit: false for manual offset control if needed
+        eachMessage: async ({ topic, partition, message, heartbeat, pause }) => {
+            await processEachMessage({ activeJobs, topic, partition, message, heartbeat, pause });
+        }
+    });
+}
 
 
 app.get('/get-subtitle/:id', async (req, res) => {
     const { id } = req.params;
     const subtitleBuffer = await GetSubtitle(id);
     res.setHeader("Content-Type", "text/vtt; charset=utf-8");
-    res.setHeader("Content-Disposition", "inline"); // make browser load directly
+    res.setHeader("Content-Disposition", "inline"); // it is to display the file directly in the browser instead of downloading it.
 
     return res.send(subtitleBuffer);
 })
 
 app.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`);
+});
+
+
+startConsumer().catch(err => {
+    console.error("Failed to start consumer:", err);
+    process.exit(1);
 });
